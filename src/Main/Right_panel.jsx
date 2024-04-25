0a7db8f3949dfd2798636b32942593992fac17c4
nikitka <nikitka@gmail.com>
2024-04-25 15:38:34 +0300

    .

--- src/Main/Right_panel.jsx
+++ src/Main/Right_panel.jsx
@@ -4,7 +4,6 @@ import { useAddlikedMutation, useGetlikedQuery } from "../redux/liked/liked";
 import { useSelector } from "react-redux";
 import { useState } from 'react';
 const Right_panel = () => {
-   const [test,settest] = useState(1)
    const { data: liked = [] } = useGetlikedQuery();
    const [addliked] = useAddlikedMutation();
    const { data: basket = [] } = useGetbasketQuery()
@@ -16,41 +15,34 @@ const Right_panel = () => {
    const goHohe = () => {
       navigate('/');
    }
-   const ttest = (item) => {
-      settest(test => test+=1);
-   }
    const add_basket = (item) => {
-      addbasket({ id : item.id, image : item.image, price : item.price})
+      addbasket({ id: item.id, image: item.image, price: item.price })
    }
+   const test = liked.length > 0 ? liked[liked.length - 1] : null;
    return (
       <>
          {!stateBasket &&
             <div style={{ display: 'flex', border: '2px solid yellow', height: '100%' }}>
                <div style={{ border: '2px solid orange', width: '42%' }}>
-                  {liked.map(item => <div key={item.id}>
-                     <div>
-                        <img src={item.image} alt="" style={{ border: '2px solid blue',width : '100%'}} />
-                     </div>
-                  </div>)}
+                  {test && <img src={test.image} style={{ border: '2px solid blue', width: '70%' }}></img>}
                </div>
-               <div style={{ border: '2px solid green', width: '58%', height: '100%'}}>
+               <div style={{ border: '2px solid green', width: '58%', height: '100%' }}>
                   <div style={{ height: '100%' }}>
-                     {liked.map(item => <div key={item.id}>
-                        <>
-                           <div style={{ display: 'flex', border: '4px solid black' }}>
-                              <img src={item.image} alt="" style={{ width: '20%', border: '1px solid blue' }} />
-                              <div style={{ display: 'flex', flexDirection: 'column',paddingLeft : '2%'}}>
-                                 <div>category : Refrigerators</div>
-                                 <div>Price : {item.price}$</div>
-                                 <button style={{ padding: '5px 0px' }}>add to liked</button>
-                                 <button style={{ padding: '5px 0px' }} onClick={() => add_basket(item)}>add to basket</button>
-                              </div>
+                     {test && (
+                        <div style={{ display: 'flex', border: '4px solid black' }}>
+                           <img src={test.image} alt="" style={{ width: '20%', border: '1px solid blue' }} />
+                           <div style={{ display: 'flex', flexDirection: 'column', paddingLeft: '2%' }}>
+                              <div>category : Refrigerators</div>
+                              <div>Price : {test.price}$</div>
+                              <button style={{ padding: '5px 0px' }}>add to liked</button>
+                              <button style={{ padding: '5px 0px' }} onClick={() => add_basket(test)}>add to basket</button>
                            </div>
-                        </>
-                     </div>)}
+                        </div>
+                     )}
                   </div>
                </div>
-            </div>}
+            </div>
+         }
          {stateBasket &&
             <div className="basket">
                {basket.length >= 1 && <div style={{ textAlign: 'center', padding: '2% 0%' }}>Your cart</div>}
@@ -61,7 +53,7 @@ const Right_panel = () => {
                   </>
                </div>}
                <div>{basket.map(item => <div key={item.id}>
-                  <div style={{ border: '4px solid orange', display: 'flex', justifyContent: 'space-between', padding: '1% 2%', margin: '3% 0%', alignItems : 'center'}}>
+                  <div style={{ border: '4px solid orange', display: 'flex', justifyContent: 'space-between', padding: '1% 2%', margin: '3% 0%', alignItems: 'center' }}>
                      <div style={{ border: '1px solid green', width: '30%', display: 'flex', alignItems: 'center' }}>
                         <div><img src={item.image} alt="" className="products_basket" /></div>
                         <div style={{ display: 'flex', flexDirection: 'column', border: '1px solid', alignContent: 'center' }}>
@@ -73,8 +65,8 @@ const Right_panel = () => {
                         <div>{item.price}$</div>
                      </div>
                      <div>
-                        <button onClick={() => ttest(item.price)}>+</button>
-                        {test}
+                        <button>+</button>
+                        {1}
                         <button>-</button>
                      </div>
                      <div>
--- src/componets/Clothes.jsx
+++ src/componets/Clothes.jsx
@@ -3,17 +3,22 @@ import { useGetpantsQuery } from '../redux/clothes/Pants'
 import { useGethatsQuery } from '../redux/clothes/Hats'
 import { useGettshirtQuery } from '../redux/clothes/Tshirt'
 import { useGethoodiesQuery } from '../redux/clothes/Hoodies'
+import { useAddlikedMutation } from '../redux/liked/liked'
 const Clothes = () => {
-  const { data: pants = [] } = useGetpantsQuery()
-  const { data: hoodies = [] } = useGethoodiesQuery()
-  const { data: tshirt = [] } = useGettshirtQuery()
-  const { data: hats = [] } = useGethatsQuery()
+  const { data: pants = [] } = useGetpantsQuery();
+  const { data: hoodies = [] } = useGethoodiesQuery();
+  const { data: tshirt = [] } = useGettshirtQuery();
+  const { data: hats = [] } = useGethatsQuery();
+  const [addliked] = useAddlikedMutation();
+  const add_at_liked = (item) => {
+    addliked({id : item.id,price : item.price,image : item.image});
+  }
   return (
     <div className='page_products'>
       <div style={{ border: '2px solid green', display: 'flex', flexDirection: 'column', marginBottom: '20px' }}>
         <div className='heading'>Pants</div>
         <div className='products'>
-          {pants.map(item => <div key={item.id}>
+          {pants.map(item => <div key={item.id} onClick={()=> add_at_liked(item)}>
             <>
               <img src={item.image} alt="" className='products_image'></img>
               <div style={{ color: 'white' }}>Price : {item.price}$</div>
